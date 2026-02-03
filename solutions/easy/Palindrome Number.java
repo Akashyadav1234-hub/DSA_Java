9c01e048be0349b6ea8623dba6024533e3155e40
Akash Yadav <akash2255yadav@gmail.com>
2026-02-04 00:19:22 +0530

    Add solution for Palindrome Number

---
+++ solutions/easy/Palindrome Number.java
@@ -0,0 +1,20 @@
+// Title: Palindrome Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/palindrome-number/
+
+            System.out.println("False");
+        else{
+        }
+            return true;
+            return false;
+        }
+    }
+}
+public class Soln{
+    public static void main(String[] args){
+        int a=121;
+        Solution obj=new Solution();
+        obj.isPalindrome(a);
+    }
+}
