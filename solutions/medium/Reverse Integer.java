7e90a9e867f968d2e895634cba6333ff65296504
Akash Yadav <akash2255yadav@gmail.com>
2026-02-04 23:46:37 +0530

    Add solution for Reverse Integer

---
+++ solutions/medium/Reverse Integer.java
@@ -0,0 +1,20 @@
+// Title: Reverse Integer
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/reverse-integer/
+
+        }
+        return (int)d;
+    }
+}
+            x=x/10;
+            d=(d*10)+y;
+            int y=x%10;
+        long d=0;
+        while(x!=0){
+    public int reverse(int x) {
+class Solution {
+import java.util.*;
+        if (d > Integer.MAX_VALUE || d < Integer.MIN_VALUE) {
+            return 0; 
+        }
