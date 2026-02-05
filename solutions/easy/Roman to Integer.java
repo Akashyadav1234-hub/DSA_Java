31a37d82212e3e70049d17b7d8fbab4f44e3a826
Akash Yadav <akash2255yadav@gmail.com>
2026-02-06 00:02:16 +0530

    Add solution for Roman to Integer

---
+++ solutions/easy/Roman to Integer.java
@@ -0,0 +1,19 @@
+// Title: Roman to Integer
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/roman-to-integer/
+
+            prevValue = currentValue;
+            }
+            } else {
+                total += currentValue;
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            int currentValue = getValue(s.charAt(i));
+
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+        int prevValue = 0;
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
