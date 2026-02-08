23ac2e2becaa6074335e4869822833e9856763c8
Akash Yadav <akash2255yadav@gmail.com>
2026-02-09 03:44:51 +0530

    Add solution for Zigzag Conversion

---
+++ solutions/medium/Zigzag Conversion.java
@@ -0,0 +1,22 @@
+// Title: Zigzag Conversion
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/zigzag-conversion/
+
+            }
+                d=-1;
+            else if(idx == numRows-1){
+            }
+            idx += d;
+            StringBuilder result=new StringBuilder();
+            for(List<Character>row:rows){
+                for(char ch : row){
+                    result.append(ch);
+                }
+            }
+    }
+                d=1;
+            if(idx==0){
+            return result.toString(); 
+        }
+}
