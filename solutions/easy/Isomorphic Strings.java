06b36d587051b4556113a20ca9a47ce2f42a6d52
Akash Yadav <akash2255yadav@gmail.com>
2026-02-25 23:49:59 +0530

    Add solution for Isomorphic Strings

---
+++ solutions/easy/Isomorphic Strings.java
@@ -0,0 +1,20 @@
+// Title: Isomorphic Strings
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/isomorphic-strings/
+
+            int[] map1=new int[256];
+            int[] map2=new int[256];
+            for(int idx=0;idx<s.length();idx++){
+                if(map1[s.charAt(idx)]!=map2[t.charAt(idx)])
+            }
+    }
+                    return false;
+                
+                map1[s.charAt(idx)]=idx+1;
+                map2[t.charAt(idx)]=idx+1;
+            return true;
+            return false;
+        if(s.length()!=t.length())
+    public boolean isIsomorphic(String s, String t) {
+class Solution {
