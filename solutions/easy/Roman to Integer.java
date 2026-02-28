f0cd036ee6bd7222a70fb23c0660e297601dfdc3
Akash Yadav <akash2255yadav@gmail.com>
2026-02-28 23:57:52 +0530

    Add solution for Roman to Integer

--- solutions/easy/Roman to Integer.java
+++ solutions/easy/Roman to Integer.java
@@ -3,17 +3,20 @@
             // Language: Java
             // Link: https://leetcode.com/problems/roman-to-integer/
 
-            prevValue = currentValue;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            int curr = map.get(s.charAt(i));
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
+
+            if (curr < next) {
+                result -= curr;
             } else {
-                total += currentValue;
-            if (currentValue < prevValue) {
-                total -= currentValue;
-            int currentValue = getValue(s.charAt(i));
+                result += curr;
+            }
+        }
 
+        return result;
+        int result = 0;
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-        int prevValue = 0;
-class Solution {
-    public int romanToInt(String s) {
-        int total = 0;
+        );
+            'L', 50, 'C', 100, 'D', 500, 'M', 1000
+            'I', 1, 'V', 5, 'X', 10,
