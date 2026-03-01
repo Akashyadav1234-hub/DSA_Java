46c183fe4342913c82236c8ebce196d99b42dff6
Akash Yadav <akash2255yadav@gmail.com>
2026-03-01 23:44:10 +0530

    Add solution for Longest Palindromic Substring

--- solutions/medium/Longest Palindromic Substring.java
+++ solutions/medium/Longest Palindromic Substring.java
@@ -3,18 +3,15 @@
             // Language: Java
             // Link: https://leetcode.com/problems/longest-palindromic-substring/
 
-        while(left>=0 && right<str.length() && str.charAt(left) == str.charAt(right)){
-    private int expandFromCenter(String str,int left,int right){
-            left--;
-            right++;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-        return right-left-1;
-    }
-}
-class mein{
-    public static void main(String[] args){
-        Solution sol=new Solution();
-        String input="babad";
-        System.out.println("Longest:"+sol.longestPalindrome(input));
+
+        return true;
     }
 }
