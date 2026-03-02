c78e00e8df097bb3a624b2afbc26af7ff8f6ab62
Akash Yadav <akash2255yadav@gmail.com>
2026-03-02 23:21:15 +0530

    Add solution for Search Insert Position

---
+++ solutions/easy/Search Insert Position.java
@@ -0,0 +1,21 @@
+// Title: Search Insert Position
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/search-insert-position/
+
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return left;
+    }
