9fd3d0cd7eee4ea901f5f73cfb300bdef42ab371
Akash Yadav <akash2255yadav@gmail.com>
2026-03-03 23:34:38 +0530

    Add solution for Search a 2D Matrix

---
+++ solutions/medium/Search a 2D Matrix.java
@@ -0,0 +1,20 @@
+// Title: Search a 2D Matrix
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int low=0, n=matrix.length, m=matrix[0].length, high=n*m-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int row=mid/m, col=mid%m;
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]<target)low=mid+1;
+            else high =mid-1;
+
+        }
+        return false;
+    }
+}
