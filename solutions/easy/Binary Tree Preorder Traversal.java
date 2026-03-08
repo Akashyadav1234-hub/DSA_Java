ba3ba022beaa29dfbf4b2dd56cfa18a558c9715a
Akash Yadav <akash2255yadav@gmail.com>
2026-03-08 15:51:13 +0530

    Add solution for Binary Tree Preorder Traversal

--- solutions/easy/Binary Tree Preorder Traversal.java
+++ solutions/easy/Binary Tree Preorder Traversal.java
@@ -3,18 +3,18 @@
             // Language: Java
             // Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
 
- */
+            res.add(curr.val);
+
+            //push right
+            TreeNode curr = st.pop();
+
+        while (!st.isEmpty()){  //print  left  right   - print  right left 
+            //pop current and add to ans
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
 class Solution {
-    List<Integer> l1 = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root==null){
-            return l1;
-        }
-        l1.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
-        
-        
-        return l1;
-    }
-}
+        //iterative
+
