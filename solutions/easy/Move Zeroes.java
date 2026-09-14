defcd477c6bcff8b146a6b26d3d38b0ee8d577eb
Akash Yadav <akash2255yadav@gmail.com>
2026-09-15 03:22:07 +0530

    Add solution for Move Zeroes

---
+++ solutions/easy/Move Zeroes.java
@@ -0,0 +1,16 @@
+// Title: Move Zeroes
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/move-zeroes/
+
+                o++;
+            }
+            else if(o>0){
+                int temp=nums[i];
+                nums[i]=0;
+            if(nums[i]==0){
+        for(int i=0;i<n;i++){
+        int o=0;
+        int n=nums.length;
+    public void moveZeroes(int[] nums) {
+class Solution {
