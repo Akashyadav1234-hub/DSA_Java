5e7536ba638fd76f3c8d0385881baf0c39f0ae11
Akash Yadav <akash2255yadav@gmail.com>
2026-09-17 18:16:03 +0530

    Add solution for Next Permutation

---
+++ solutions/medium/Next Permutation.java
@@ -0,0 +1,14 @@
+// Title: Next Permutation
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/next-permutation/
+
+            return ;
+        }
+        for(int j=n-1;j>=0;j--){
+            if(nums[j]>nums[breakidx]){
+            reverse(0, n-1, nums);
+        if(breakidx==-1){
+        }
+            i--;
+            }
