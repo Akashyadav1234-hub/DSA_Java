0776b51bd124a9e75090adfb4d323e546fc19aa2
Akash Yadav <akash2255yadav@gmail.com>
2026-09-20 22:49:08 +0530

    Add solution for Longest Consecutive Sequence

---
+++ solutions/medium/Longest Consecutive Sequence.java
@@ -0,0 +1,13 @@
+// Title: Longest Consecutive Sequence
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-consecutive-sequence/
+
+
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+}
