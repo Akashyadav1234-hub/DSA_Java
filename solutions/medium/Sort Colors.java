abe1fca911ecedeb8905db957f752e2e4f0c2571
Akash Yadav <akash2255yadav@gmail.com>
2026-09-25 22:30:45 +0530

    Add solution for Sort Colors

--- solutions/medium/Sort Colors.java
+++ solutions/medium/Sort Colors.java
@@ -3,18 +3,13 @@
             // Language: Java
             // Link: https://leetcode.com/problems/sort-colors/
 
-                mid++;
-            }
-            else if(nums[mid]==2){
-                Swap(nums,mid,high);
-            }
-                high--;
         }
-    }
-            else if(nums[mid]==1){
-    void Swap(int[] nums,int num1,int num2){
-        int temp=nums[num1];
-        nums[num1]=nums[num2];
-        nums[num2]=temp;
-    }
-}
+        while(c1>0){
+            nums[j++]=1;
+        }
+        while(c2>0){
+            nums[j++]=2;
+            c0--;
+            c1--;
+        }
+            c2--;
